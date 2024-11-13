7e074e262985b6ae1efca1556c1f8fabafac5d11
Elise <elise.maaiza33@gmail.com>
2024-11-13 11:11:22 +0100

    Updated repo

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
+
 import { Picture } from "src/entities/Picture";
-=======
->>>>>>> ab987672d329d73bcd3e28f11ec1258ed549b79b
 import { Ad } from "../entities/Ad";
 import { Category } from "../entities/Category";
 import { Tag } from "../entities/Tag";
@@ -10,11 +8,7 @@ import { DataSource } from "typeorm";
 export const dataSourceGoodCorner = new DataSource({
    database: "good_corner.sqlite",
    type: "sqlite",
-<<<<<<< HEAD
    entities: [Ad, Category, Tag, Picture],
-=======
-   entities: [Ad, Category, Tag],
->>>>>>> ab987672d329d73bcd3e28f11ec1258ed549b79b
    synchronize: true,
    logging: ["error", "query"]
 });
--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,8 @@ import { validate } from "class-validator";
 import { Category } from "./entities/Category";
 import { Like } from "typeorm";
 import { Tag } from "./entities/Tag";
-<<<<<<< HEAD
 import { Picture } from "./entities/Picture";
-=======
->>>>>>> ab987672d329d73bcd3e28f11ec1258ed549b79b
+
 
 const app = express();
 const port = 3000;
@@ -54,31 +52,20 @@ app.get("/ads", async (req, res) => {
 
 // GET AD BY ID
 app.get("/ads/:id", async (req, res) => {
-<<<<<<< HEAD
-  const adById = await Ad.findOneByOrFail(
-    { id: parseInt(req.params.id) }
-  );
-=======
   const adById = await Ad.findOneByOrFail({ id: parseInt(req.params.id) });
->>>>>>> ab987672d329d73bcd3e28f11ec1258ed549b79b
   res.send(adById);
 });
 
 // CREATE AD
 app.post("/ads", async (req, res) => {
   const newAd = new Ad();
-<<<<<<< HEAD
-
   const pictures: Picture[] = [];
   req.body.pictures.forEach(async (el: string) => {
     const newPicture = new Picture();
     newPicture.url = el;
     pictures.push(newPicture);
   });
-
-=======
   
->>>>>>> ab987672d329d73bcd3e28f11ec1258ed549b79b
   newAd.title = req.body.title;
   newAd.description = req.body.description;
   newAd.owner = req.body.owner;
@@ -91,13 +78,10 @@ app.post("/ads", async (req, res) => {
   newAd.category = req.body.category ? req.body.category : 6;
   newAd.tags = req.body.tags ? req.body.tags : "";
 
-<<<<<<< HEAD
   if (pictures.length > 0) {
     newAd.pictures = pictures;
   }
 
-=======
->>>>>>> ab987672d329d73bcd3e28f11ec1258ed549b79b
   const errors = await validate(newAd);
   if (errors.length > 0) {
     console.log(errors);
